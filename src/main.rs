f1f154621811a690b50bcbb14a4558d23b0bc2d5
Sean Collins <sean.collins@outlook.com>
2024-09-12 17:34:51 -0700

    Bring it all together with an add command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+mod cli;
+mod db;
+mod models;
+
+use cli::build_cli;
+use rusqlite::Connection;
+
+fn main() -> rusqlite::Result<()> {
+    let matches = build_cli().get_matches();
+
+    // Connect to the SQLite database
+    let conn = Connection::open("affirmations.db")?;
+
+    match matches.subcommand() {
+        Some(("add", sub_m)) => {
+            let title = sub_m.get_one::<String>("title").expect("Title is required");
+            let description = sub_m
+                .get_one::<String>("description")
+                .expect("Description is required");
+            db::add_affirmation(&conn, title, description)?;
+            println!("Added {}, {} to the database.", title, description);
+        }
+        Some(("list", _)) => {
+            db::list_affirmations(&conn)?;
+        }
+        _ => unreachable!(),
+    }
+
+    Ok(())
 }
