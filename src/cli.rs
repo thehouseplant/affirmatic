a7d5f2c07acc196210eeb48858f5fec4bc27ed30
Sean Collins <sean.collins@outlook.com>
2024-09-12 17:16:35 -0700

    Add commands to add and list affirmations

---
+++ src/cli.rs
@@ -0,0 +1,22 @@
+use clap::{Arg, Command};
+
+pub fn build_cli() -> Command<'static> {
+    Command::new("affirmator")
+        .version("0.1.0")
+        .author("Sean Collins <sean.collins@outlook.com")
+        .subcommand(
+            Command::new("add")
+                .about("Add a new affirmation")
+                .arg(
+                    Arg::new("title")
+                        .required(true)
+                        .help("Title for your affirmation"),
+                )
+                .arg(
+                    Arg::new("description")
+                        .required(true)
+                        .help("Description of your affirmation"),
+                ),
+        )
+        .subcommand(Command::new("list").about("List all of your affirmations"))
+}
